5dccfd46456b19e37130c89785328437a752d0d8
rf-blanco1140 <rf-blanco1140@users.noreply.github.com>
2023-05-22 06:59:26 +0200

    Added interactions for door and axe & fixed Y-sort in scene 2

--- TheTownVisitedByOgres/Scenes/Game Components/interact_area.gd
+++ TheTownVisitedByOgres/Scenes/Game Components/interact_area.gd
@@ -4,6 +4,9 @@ class_name Interactable extends Area2D
 @export var interactable_label = "none"
 @export var dialog_resoruce : DialogueResource
 @export var dialog_start = "first_intearction"
+@export var num_interact_total = 1
+@export var num_interact_curr = 0
 
 func interact() -> void:
-	DialogueManager.show_example_dialogue_balloon(dialog_resoruce, dialog_start)
+	if num_interact_curr < num_interact_total:
+		DialogueManager.show_example_dialogue_balloon(dialog_resoruce, dialog_start)
